fdfbf15bdfbb427bcd1193aec378cfc0cc1007e9
sahil-soni18 <100425303+sahil-soni18@users.noreply.github.com>
2025-01-19 15:57:18 +0530

    Learned to create REST APIs in Spring Boot...

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controllers/JournalEntryController.java
@@ -0,0 +1,42 @@
+package net.engineeringdigest.journalApp.controllers;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/Journals")      // Prefix for all the APIs in this class...
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping("/get-all")         // Not Necessary to write anything after get mapping annotation, then this will be treated as Get + http://localhost:8080/Journal
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping("/create")
+    public Boolean createEntry(@RequestBody JournalEntry newEntry) {
+        journalEntries.put(newEntry.getId(), newEntry);
+        return true;
+    }
+
+    @GetMapping("/id/{JId}")
+    public JournalEntry findById (@PathVariable Long JId) {
+        return journalEntries.get(JId);
+    }
+
+    @DeleteMapping("/id/{JId}")
+    public JournalEntry deleteById (@PathVariable Long JId) {
+        return journalEntries.remove(JId);
+    }
+
+    @PutMapping("/id/{JId}")
+    public JournalEntry updateById (@PathVariable Long JId, @RequestBody JournalEntry updatedEntry) {
+        return journalEntries.put(JId, updatedEntry);
+    }
+}
